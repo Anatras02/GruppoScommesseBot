aaeb51b4924db6ba355dcfa269afce68f08ca176
Anatras02 <alessio.balderi@adraise.it>
2022-01-05 21:26:38 +0100

    Aggiunto blocco gruppi non autorizzati

--- ScommesseBot.py
+++ ScommesseBot.py
@@ -1,5 +1,13 @@
-from pyrogram import Client, filters
+from pyrogram import Client, filters, StopPropagation
+
+from config.variabili import chatScommesse
 
 plugins = dict(root="plugins")
 app = Client("ScommesseBot", config_file="config/config.ini", plugins=plugins).run()
 
+
+@app.on_message(group=-1)
+def gruppo_non_valido(_, message):
+    if message.chat.id not in chatScommesse:
+        message.reply("Gruppo non abilitato, contatta @Anatras02 se credi si tratti di un errore")
+        raise StopPropagation
